95557891e4d0c56831eb96e0ac7d80ece426091b
Jaid <jaid.jsx@gmail.com>
2019-02-09 08:37:09 +0100

    Improved debug output
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/transformTsd.js
+++ src/transformTsd.js
@@ -1,6 +1,8 @@
 const moduleBlockRegex = /(declare module [^\n]+)(.+?)(\n})/gs
 const moduleFieldRegex = /^([\t ]*)(type|function|interface|var|const|class) +(\w+)/gm
 
+const debug = require("debug")("jsdoc-tsd-webpack-plugin")
+
 const transformField = (type, name) => {
   if (name === "default") {
     return `export default ${type}`
@@ -10,6 +12,10 @@ const transformField = (type, name) => {
 }
 
 export default text => text.replace(moduleBlockRegex, (blockMatch, blockPrefix, blockContent, blockSuffix) => {
-  const newContent = blockContent.replace(moduleFieldRegex, (fieldMatch, fieldPrefix, fieldType, fieldName) => fieldPrefix + transformField(fieldType, fieldName))
+  const newContent = blockContent.replace(moduleFieldRegex, (fieldMatch, fieldPrefix, fieldType, fieldName) => {
+    const transformedField = transformField(fieldType, fieldName)
+    debug(`Transformed "${fieldType} ${fieldName}" to "${transformedField}"`)
+    return fieldPrefix + transformField
+  })
   return blockPrefix + newContent + blockSuffix
 })
